a4cd43e995dd2c26d5eb488bf5cb9f23e81769e5
Mahmoud-ms1996 <mahmoud.masri1996@gmail.com>
2022-03-20 21:07:26 +0200

    adding files

---
+++ js/script.js
@@ -0,0 +1,92 @@
+let menu   = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// scrollSpy by ID
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    // scrollSpy by ID
+    section.forEach(sec =>{
+
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+  
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+    });
+}
+
+// search-icon
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+// home-slider
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+// review-slider
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// loading screen
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
